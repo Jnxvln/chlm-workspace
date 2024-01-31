64e6e15e7898bbd1668c9d499fec188865cf387f
Jnxvln <jnx1017@gmail.com>
2024-01-31 15:40:13 -0600

    More work on DriversTable and begin work on Dialog

--- app/components/Dialog/Dialog.tsx
+++ app/components/Dialog/Dialog.tsx
@@ -1,16 +1,31 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, ReactElement } from "react";
 import styles from "./Dialog.module.scss";
 
 type TDialog = {
   title?: String;
-  content?: String;
+  content?: String | ReactElement<any, any> | JSX.Element | undefined | null;
   footer?: "ok" | "cancel" | "yes-no" | "yes-no-ok" | "yes-no-cancel";
   callbacks?: [Function] | undefined | null;
+  show?: Boolean;
 };
 
-export default function Dialog({ title, content, footer }: TDialog) {
-  const [visible, setVisible] = useState(true);
+export default function Dialog({
+  title,
+  content,
+  footer,
+  show,
+  callbacks,
+}: TDialog) {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (show) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }, [show]);
 
   const renderFooter = () => {
     switch (footer) {
@@ -20,7 +35,12 @@ export default function Dialog({ title, content, footer }: TDialog) {
             <button
               type="button"
               className={`${styles.button} ${styles.btn_ok}`}
-              onClick={() => setVisible(false)}
+              onClick={(e) => {
+                setVisible(false);
+                if (callbacks && callbacks.length > 0) {
+                  callbacks[0]();
+                }
+              }}
             >
               Ok
             </button>
@@ -32,6 +52,7 @@ export default function Dialog({ title, content, footer }: TDialog) {
             <button
               type="button"
               className={`${styles.button} ${styles.btn_cancel}`}
+              onClick={() => setVisible(false)}
             >
               Cancel
             </button>
@@ -43,6 +64,12 @@ export default function Dialog({ title, content, footer }: TDialog) {
             <button
               type="button"
               className={`${styles.button} ${styles.btn_default}`}
+              onClick={(e) => {
+                setVisible(false);
+                if (callbacks && callbacks.length > 0) {
+                  callbacks[0]();
+                }
+              }}
             >
               Uh oh!
             </button>
--- app/drivers/DriversTable/DriversTable.tsx
+++ app/drivers/DriversTable/DriversTable.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState, useEffect } from "react";
 import dayjs, { Dayjs } from "dayjs";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getAllDrivers } from "@/app/controllers/DriverController";
@@ -6,6 +7,7 @@ import ErrorMessage from "@/app/components/ErrorMessage/ErrorMessage";
 import { TDriver } from "@/app/Types";
 import CalendarReveal from "@/app/components/CalendarReveal/CalendarReveal";
 import Loading from "@/app/components/Loading/Loading";
+import Dialog from "../../components/Dialog/Dialog";
 import styles from "./DriversTable.module.scss";
 
 export default function DriversTable() {
@@ -18,9 +20,218 @@ export default function DriversTable() {
     queryFn: getAllDrivers,
   });
 
+  const [form, setForm] = useState({
+    id: "",
+    firstName: "",
+    lastName: "",
+    defaultTruck: "",
+    endDumpPayRate: 0,
+    flatBedPayRate: 0,
+    ncPayRate: 0,
+    dateHired: undefined,
+    dateReleased: undefined,
+    isActive: undefined,
+  });
+
+  const [selectedDriver, setSelectedDriver] = useState<
+    TDriver | undefined | null
+  >();
+
+  const [showEditDialog, setShowEditDialog] = useState(false);
+
   const PerLabel = ({ label }: { label: string }) => {
     return <span className={styles.perLabel}>/{label}</span>;
   };
+
+  const editDialogContent = (driverArg: TDriver | undefined | null) => {
+    if (!driverArg) return null;
+    return (
+      <div>
+        <header>
+          <h3 className="text-xl font-bold">
+            {driverArg.firstName} {driverArg.lastName}
+          </h3>
+        </header>
+        <section>
+          <form>
+            <table className={styles.editDriverTable}>
+              <tbody>
+                {/* ID */}
+                <tr>
+                  <td>ID: </td>
+                  <td>
+                    <div>
+                      <input
+                        type="text"
+                        tabIndex={-1}
+                        value={form.id}
+                        readOnly
+                      />
+                    </div>
+                  </td>
+                </tr>
+
+                {/* First Name */}
+                <tr>
+                  <td>First Name: </td>
+                  <td>
+                    <div>
+                      <input
+                        type="text"
+                        value={form.firstName}
+                        placeholder="First Name"
+                      />
+                    </div>
+                  </td>
+                </tr>
+
+                {/* Last Name */}
+                <tr>
+                  <td>Last Name: </td>
+                  <td>
+                    <div>
+                      <input
+                        type="text"
+                        value={form.lastName}
+                        placeholder="Last Name"
+                      />
+                    </div>
+                  </td>
+                </tr>
+
+                {/* Default Truck */}
+                <tr>
+                  <td>Default Truck: </td>
+                  <td>
+                    <div>
+                      <input
+                        type="text"
+                        value={form.defaultTruck}
+                        placeholder="Default Truck"
+                      />
+                    </div>
+                  </td>
+                </tr>
+
+                {/* End Dump Pay Rate */}
+                <tr>
+                  <td>End Dump Rate: </td>
+                  <td>
+                    <div>
+                      <input
+                        type="number"
+                        min={0}
+                        step={0.01}
+                        value={form.endDumpPayRate}
+                      />
+                    </div>
+                  </td>
+                </tr>
+
+                {/* Flat Bed Pay Rate */}
+                <tr>
+                  <td>Flat Bed Rate: </td>
+                  <td>
+                    <div>
+                      <input
+                        type="number"
+                        min={0}
+                        step={0.01}
+                        value={form.flatBedPayRate}
+                      />
+                    </div>
+                  </td>
+                </tr>
+
+                {/* NC Pay Rate */}
+                <tr>
+                  <td>NC Rate: </td>
+                  <td>
+                    <div>
+                      <input
+                        type="number"
+                        min={0}
+                        step={0.01}
+                        value={form.ncPayRate}
+                      />
+                    </div>
+                  </td>
+                </tr>
+
+                {/* Date Hired */}
+                <tr>
+                  <td>Hired: </td>
+                  <td>
+                    <div>
+                      <input
+                        type="date"
+                        value={form.dateHired}
+                        placeholder="Date Hired"
+                        style={{ width: "100%" }}
+                      />
+                    </div>
+                  </td>
+                </tr>
+
+                {/* Date Released */}
+                <tr>
+                  <td>Released: </td>
+                  <td>
+                    <div>
+                      <input
+                        type="date"
+                        value={form.dateReleased}
+                        placeholder="Date Released"
+                        style={{ width: "100%" }}
+                      />
+                    </div>
+                  </td>
+                </tr>
+
+                {/* Is Active */}
+                <tr>
+                  <td>Active: </td>
+                  <td>
+                    <div>
+                      <input type="checkbox" checked={form.isActive} />
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </form>
+        </section>
+      </div>
+    );
+  };
+
+  const onEdit = (driverArg: TDriver) => {
+    console.log(driverArg);
+    setSelectedDriver(driverArg);
+    setShowEditDialog(true);
+  };
+
+  const onEditCallback = () => {
+    setShowEditDialog(false);
+  };
+
+  useEffect(() => {
+    if (selectedDriver) {
+      setForm((prevState) => ({
+        ...prevState,
+        id: selectedDriver.id,
+        firstName: selectedDriver.firstName,
+        lastName: selectedDriver.lastName,
+        defaultTruck: selectedDriver.defaultTruck,
+        endDumpPayRate: parseFloat(selectedDriver.endDumpPayRate).toFixed(2),
+        flatBedPayRate: parseFloat(selectedDriver.flatBedPayRate).toFixed(2),
+        ncPayRate: parseFloat(selectedDriver.ncPayRate).toFixed(2),
+        dateHired: dayjs(selectedDriver.dateHired).format("YYYY-MM-DD"),
+        dateReleased: dayjs(selectedDriver.dateReleased).format("YYYY-MM-DD"),
+        isActive: selectedDriver.isActive,
+      }));
+    }
+  }, [selectedDriver]);
 
   if (error) return <ErrorMessage name={error.name} message={error.message} />;
 
@@ -28,6 +239,12 @@ export default function DriversTable() {
 
   return (
     <div className={styles.wrapper}>
+      <Dialog
+        title="Edit Driver"
+        content={editDialogContent(selectedDriver)}
+        show={showEditDialog}
+        callbacks={[onEditCallback]}
+      />
       <table className={styles.table}>
         <thead>
           <tr>
@@ -100,7 +317,11 @@ export default function DriversTable() {
 
               <td>
                 <div className="flex gap-2">
-                  <button type="button" className={styles.editBtn}>
+                  <button
+                    type="button"
+                    className={styles.editBtn}
+                    onClick={(e) => onEdit(driver)}
+                  >
                     E
                   </button>
                   <button type="button" className={styles.deleteBtn}>
